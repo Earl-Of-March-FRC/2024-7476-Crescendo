b9998955c8386b97cc4fbbedb84da38b2218f270
EomLions <eomlions@gmail.com>
2024-03-21 19:21:26 -0400

    Set shooter to break on neutral pt3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -82,7 +82,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
 
-    m_robotContainer.shooterSub.setBrakeMode();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
